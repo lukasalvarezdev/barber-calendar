f23d675a9621492376d71e09a6a3e1c7528b0662
Lukas Alvarez <53347228+lukasalvarezdev@users.noreply.github.com>
2022-07-04 17:50:38 -0500

    fix: remove cypress config

--- app/api.ts
+++ app/api.ts
@@ -2,11 +2,11 @@ import { supabaseClient } from '~/supabase.server';
 import type { IAppointment, IBarber } from './types';
 
 function getAllBarbers() {
-  return supabaseClient.from<IBarber>('barbers').select('*').order('id', { ascending: true });
+  return supabaseClient.from<IBarber>('barber').select('*').order('id', { ascending: true });
 }
 
 function getBarberById(id: string) {
-  return supabaseClient.from<IBarber>('barbers').select('*').eq('id', id).single();
+  return supabaseClient.from<IBarber>('barber').select('*').eq('id', id).single();
 }
 
 function getAllAppointments() {
