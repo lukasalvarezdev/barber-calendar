0d2d6e4605af3fef6992f0ea60f0d3364a33c511
Danilo Morales <Danilo.Morales@overactive.com>
2022-07-04 18:21:32 -0500

    fix: styles to barber image

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,14 +1,16 @@
-import { Link, Outlet, useLoaderData } from '@remix-run/react';
-import { json } from '@remix-run/node';
-import type { LoaderFunction } from '@remix-run/node';
-import { getAllBarbers } from '~/api';
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import type { LoaderFunction } from "@remix-run/node";
+import { getAllBarbers } from "~/api";
 
-type LoaderData = { barbers: NonNullable<Awaited<ReturnType<typeof getAllBarbers>>['data']> };
+type LoaderData = {
+  barbers: NonNullable<Awaited<ReturnType<typeof getAllBarbers>>["data"]>;
+};
 
 export const loader: LoaderFunction = async () => {
   const { data: barbers, error } = await getAllBarbers();
 
-  if (error || !barbers) throw new Error('Failed to load barbers');
+  if (error || !barbers) throw new Error("Failed to load barbers");
 
   return json<LoaderData>({ barbers });
 };
@@ -19,11 +21,15 @@ export default function Index() {
   return (
     <div>
       <div className="flex gap-4">
-        {barbers.map(b => (
+        {barbers.map((b) => (
           <Link key={b.id} className="flex-1" to={`${b.id}`}>
             <h2>{b.name}</h2>
 
-            <img src={b.imageUrl} alt={`${b.name}'s`} className="w-1/2 max-h-max rounded rounded-lg h-36" />
+            <img
+              src={b.imageUrl}
+              alt={`${b.name}'s`}
+              className="w-1/2 max-h-max rounded aspect-square"
+            />
           </Link>
         ))}
       </div>
